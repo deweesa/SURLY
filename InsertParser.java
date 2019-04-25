7a87571f8c8dadb6d66af81ce522a1e2af02c38a
deweesa <deweesa@wwu.edu>
2019-04-25 13:40:40 -0700

    Up to date insert parser

--- InsertParser.java
+++ InsertParser.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 /* Asa DeWeese
  * SURLY 0
  * CSCI 330, 12:00pm
@@ -20,7 +22,6 @@ public class InsertParser{
    {
       int count = 0, index = 0;
       String attr = input.substring(input.indexOf(' '));
-      //System.out.println(attr);
       
       while(attr.charAt(index) != ';')
       {
@@ -45,12 +46,49 @@ public class InsertParser{
    
    public Tuple parseTuple()
    {
-      Attribute attr = new Attribute();
-      attr.setName(this.parseRelationName());
-      
+      LinkedList<String> values = new LinkedList<>();
+      String currentValue = "";
+      String attributes = input.substring(input.indexOf(' '));
+
+      //System.out.println(attributes);
       int i = 0;
-      while(input.charAt(i) != ';')
+      while(attributes.charAt(i) != ';')
       {
-         
+          while(attributes.charAt(i) == ' ') i++; //skip spaces
+
+          if(attributes.charAt(i) == '\''){
+              i++;
+              while(attributes.charAt(i) != '\'' && attributes.charAt(i) != ';')
+              {
+                  currentValue += attributes.charAt(i);
+                  i++;
+              }
+              i++;
+
+              values.push(currentValue);
+              currentValue = "";
+          }
+
+          if(attributes.charAt(i) != ' ' && attributes.charAt(i) != '\'') {
+              while (attributes.charAt(i) != ' ' && attributes.charAt(i) != ';') {
+                  currentValue += attributes.charAt(i);
+                  i++;
+              }
+
+              values.push(currentValue);
+              currentValue = "";
+          }
+      }
+
+      Tuple tuple = new Tuple();
+      for(i = 0; i < values.size(); i ++)
+      {
+          AttributeValue attr = new AttributeValue(values.get(i));
+          tuple.push(attr);
+          System.out.print(attr.getValue()+":");
+
+      }
+       System.out.println();
+      return tuple;
    }
 }
